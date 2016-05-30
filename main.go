50d3b2f43a164074ed0f427f051810f3f90b5e7d
nel215 <otomo.yuhei@gmail.com>
2016-05-30 22:18:52 +0900

    implemented db access

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"fmt"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
@@ -17,15 +18,32 @@ func New() (*Service, error) {
 		return nil, err
 	}
 	defer db.Close()
-	db.Put([]byte(CURRENT_TERM), []byte("0"), nil)
+	err = db.Put([]byte(CURRENT_TERM), []byte("0"), nil)
+	if err != nil {
+		return nil, err
+	}
 	return &Service{}, nil
 }
 
 type RequestVoteArgs struct {
 }
+type RequestVoteResponse struct {
+}
 type AppendEntriesArgs struct{}
 
-func (s *Service) RequestVote(args *RequestVoteArgs, reply *string) error {
+func (s *Service) RequestVote(args *RequestVoteArgs, reply *RequestVoteResponse) error {
+	db, err := leveldb.OpenFile("./test_db", nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	currentTerm, err := db.Get([]byte(CURRENT_TERM), nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(currentTerm))
+	reply = &RequestVoteResponse{}
+
 	return nil
 }
 
--- raft_test.go
+++ raft_test.go
@@ -1,6 +1,8 @@
 package raft
 
 import (
+	"net"
+	"net/http"
 	"net/rpc"
 	"testing"
 )
@@ -8,7 +10,27 @@ import (
 func TestMain(t *testing.T) {
 	s, err := New()
 	if err != nil {
-		t.Errorf("%s", err)
+		t.Errorf("%s", err.Error())
 	}
 	rpc.Register(s)
+	rpc.HandleHTTP()
+	l, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+	go http.Serve(l, nil)
+
+	cli, err := rpc.DialHTTP("tcp", ":1234")
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	args := &RequestVoteArgs{}
+	var reply RequestVoteResponse
+	err = cli.Call("Service.RequestVote", args, &reply)
+
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
 }
